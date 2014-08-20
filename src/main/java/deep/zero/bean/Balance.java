35fcdede6b3973025061d222093fb149ed6fa2f3
coco <apple@apple-PC>
2014-08-20 09:58:02 +0800

    Balance

--- src/main/java/deep/zero/bean/Balance.java
+++ src/main/java/deep/zero/bean/Balance.java
@@ -1,12 +1,85 @@
 package deep.zero.bean;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import deep.sys.bean.User;
+
 /**
- * 账户余额类用以记录每个账户的余额
+ * 账户余额类用以记录每个账户的余额 
  * @author Administrator
  *
  */
+@Entity
+@Table(name="z_balance")
 public class Balance {
 	private Long userId;
-	private Long id;
-	private float	balance;
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public BigDecimal getBalance() {
+		return balance;
+	}
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+	public Date getTransferTime() {
+		return transferTime;
+	}
+	public void setTransferTime(Date transferTime) {
+		this.transferTime = transferTime;
+	}
+	public BigDecimal getAmount() {
+		return amount;
+	}
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public Boolean getIo() {
+		return io;
+	}
+	public void setIo(Boolean io) {
+		this.io = io;
+	}
+	public String getOpp() {
+		return opp;
+	}
+	public void setOpp(String opp) {
+		this.opp = opp;
+	}
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;  
+	private BigDecimal	balance;
+	//转款时间
+	private	Date transferTime;
+	//转款金额
+	private BigDecimal	amount;
+//	备注
+	private String	remark;
+	//出入标志
+	private Boolean	io;
+	//目标或者来源
+	private String 	opp;
 }
