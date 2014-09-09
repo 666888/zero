1b336b9f3da859d05f997d05df31d1424579c763
ckck <710001541@qq.com>
2014-09-09 09:12:09 +0800

    账户信息repo

--- src/main/java/deep/zero/repo/AccountRepo.java
+++ src/main/java/deep/zero/repo/AccountRepo.java
@@ -1,9 +1,22 @@
 package deep.zero.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import deep.zero.bean.Account;
 
 public interface AccountRepo extends JpaRepository<Account, Long>{
+	
+	@Query(value="select p from Account p where p.playerId =?1 ")
+	List<Account> getAccountByPlayer(Long playerId);
+
+	@Query(value="select p from Account p where p.playerId =?1 AND p.name = ?2 ")
+	Account getAccountByPlayerIdAndName(Long playId, Long name);
+//	@Modifying
+//	@Query(value="update Account a set a. where p.playerId =?1 AND p.name = ?2 ")
+//	void updateAccount(Account account);
 
 }
