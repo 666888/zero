e6b2c65193ec62f78db5418573f1b023fa4481a1
ckck <710001541@qq.com>
2014-09-09 09:11:17 +0800

    玩家账户信息操作与实现

--- src/main/java/deep/zero/svc/AccountSvc.java
+++ src/main/java/deep/zero/svc/AccountSvc.java
@@ -1,10 +1,34 @@
 package deep.zero.svc;
+
+import java.util.List;
+
+import deep.zero.bean.Account;
+
 /**
- * 1.玩家第一次进入一个游戏G创建一个G账户
- * 2.
- * @author Administrator
+ * 此类用以描述玩家的账户信息，由于一个玩家可以有多个游戏，每个游戏都有其账户
+ * 此外玩家还有一个中心账户定义为-1
+ * 涉及到的业务主要有
+ * 1.玩家注册完成后产生玩家中心账户
+ * 2.玩家第一次进入一个游戏创建该游戏对应账户
+ * 3.冻结中心账户
+ * 3.通过玩家查询所有的账户
+ * 
+ * 所需的业务接口操作如下
+ * 1.创建账户
+ * 2冻结中心账户
+ * 3.通过玩家查询账户列表
+ * @author chenlj
  *
  */
 public interface AccountSvc {
+//	创建一个账户
+	public void createAccount(Account account);
+	
+//	冻结中心账户
+	public void freezenAccount(Long playId);
+	
+//	通过玩家查询账户列表
+	public List<Account> getAccountByPlayer(Long playerId);
+	 
 
 }
--- src/main/java/deep/zero/svc/AccountSvcImpl.java
+++ src/main/java/deep/zero/svc/AccountSvcImpl.java
@@ -1,5 +1,7 @@
 package deep.zero.svc;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +16,28 @@ public class AccountSvcImpl implements AccountSvc{
 	@Autowired
 	private AccountRepo ar;
 	
-	@Transactional
-	public void add(Player player,Long name) {
-		Account account=new Account();
-		account.setName(name);
-		account.setPlayerId(player.getId());
+//	@Transactional
+//	public void add(Player player,Long name) {
+//		Account account=new Account();
+//		account.setName(name);
+//		account.setPlayerId(player.getId());
+//		ar.save(account);
+//	}
+//	玩家首次进入游戏创建游戏账户
+	public void createAccount(Account account) {
 		ar.save(account);
+	}
+//冻结账户
+	public void freezenAccount(Long playId) {
+		// TODO Auto-generated method stub
+		Account account=ar.getAccountByPlayerIdAndName(playId,-1L);
+		account.setIsFreezen(true);
+		ar.saveAndFlush(account);
+	}
+//根据玩家获取账户列表
+	public List<Account> getAccountByPlayer(Long playerId) {
+		// TODO Auto-generated method stub
+		return ar.getAccountByPlayer(playerId);
 	}
 	
 }
