edbef62bdf17fd4307343c10df9268ee9fcc54c8
gpc <apple@apple-PC>
2014-09-12 16:56:54 +0800

    constants.java

---
+++ src/main/java/deep/tool/Constants.java
@@ -0,0 +1,8 @@
+package deep.tool;
+
+public class Constants {
+    
+     public static   final String[] transType={"0","1","2","-1","-2"};// 0:卡到主账户；  1：自有账户到游戏账户或游戏账户到游戏账户；  2：自有账户到卡 ; -1:转出； -2：转入
+     
+     
+}
