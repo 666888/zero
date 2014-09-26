532d34fa0ce2bf9556742240f7a4f073e81c10c3
ckck <710001541@qq.com>
2014-09-26 09:49:41 +0800

    添加变量

--- src/main/java/deep/tool/Constants.java
+++ src/main/java/deep/tool/Constants.java
@@ -1,7 +1,23 @@
 package deep.tool;
 
 public class Constants {
-    
+     public static final String  SERVER_INDEX_URL="http://localhost";
+     public static final String  SERVER_PAY_INDEX="http://127.0.0.1";
+     public static final String  LOGIN_URL_REDIRECT="/managemall/login.html";
+     public static final String  ActionServer_URL="http://localhost"; 
+     public static final String  ImageServer_URL= "http://localhost";
+     //public static final String  LOGIN_URL_INDEX="/imoon/index.jsp";
+     
+     public static final String  LOGIN_URL_INDEX="/mobilemall/index.jsp";
+     public static final String  LOGIN_URL_MANAGE="/managemall/index.html";
+     public static final String  LOGIN_URL_LOGIN="/managemall/login.html";//电脑后台注册后跳到的登录界面
+     public static final String  LOGIN_URL_LOGINMOBILE="/mobilemall/login.jsp";//手机注册后跳到的登录界面
+     public static final String  SESSIONKEY="member";
+     public static final String  ENCODING_TYPE="utf-8";
+     public static final String  GOTO_URL="backurl";
+     public static final String  WEB_NAME="潮流前线后台";
+     public static final String  WAP_NAME="潮流前线商城";
+     public static final String  DEFAULT_STORE_SUFFIXNAME="的商店";
      public static   final String[] transType={"0","1","2","-1","-2"};// 0:卡到主账户；  1：自有账户到游戏账户或游戏账户到游戏账户；  2：自有账户到卡 ; -1:转出； -2：转入
      
      
